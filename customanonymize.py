1ee1b1c6b964e3e7ca5694fca5751cb328e40bc8
The Yeti <theyetinel@gmail.com>
2017-03-07 10:21:42 -0800

    Got the real to fake mapping

--- customanonymize.py
+++ customanonymize.py
@@ -7,6 +7,8 @@
 from fakecompanylist import *
 import sys
 from time import gmtime, strftime
+import random
+
 ## Want to make a service that allows people to
 # 1. specify what columns to obfuscate
 # 2. pick what database / project / table to insert into
@@ -24,7 +26,7 @@ from time import gmtime, strftime
 
 ## Randomly order fake data
 def random_value(fakedatalist):
-	return random.sample(fakedatalist,len(fakedatalist))
+	return random.choice(fakedatalist)
 
 
 
@@ -37,45 +39,46 @@ def enough_fake_data(real_data,fake_data):
 	if real_data_size > fake_data_size:
 		print 'Not enough random values'
 		syslog.syslog(syslog.LOG_ERR, "Not enough fake values to fill the data in, get more fake data for this" + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-		return
+	return
 
 
 
-## Map 1 column to list of fake names
-## need to know the column they want to map to
-## once this is used, I'm assuming that the data has 
-## been randomized, correctly selected, and that there
-## is enough fake data to proceed. Also assuming that data
-## is an array of 1 dimensional json, and the fake data 
-## is a list 
-def company_name(real_data, fake_data):
-	## Making a set to get UNIQUE values of the data
-	## then building a list so I can build a dictionary
-	## mapping real values to fake values.  This will also
-	## allow profiling later. 
+## Faker method for this sucked so here we are
+## I hardcoded the fakecompanies list in the args.
+## Didn't really seem like a reason to paramaterize
+## tha, other wise it would be x_name or something
+def company_name():
+	fake_rand = random_value(fakecompanies)
+	return fake_rand
+	
+
+## Want dictionary where keys are the real 
+## data values and the values are the 
+## anonymized data.
+def mapping(real_data, col_name, fake_meth):
+# 
+	final_mapping = {}
+# 
 	unique_values = set([])
-
+# 
 	for row in real_data:
-		unique_values.add(row[attribute_name])
-
+		unique_values.add(row[col_name])
+# 
 	real_values = list(unique_values)
-	fake_values = fakecompanies
-	data_mapping = {}
-
+# 
 	for value in real_values:
-		data_mapping[value] = fake_values[0]
-		del fake_values[0]
-
-	return data_mapping
-
+		final_mapping[value] = fake_meth()
+# 
+	return final_mapping
 
 
 
 
 
-
-
-
+1. Get Json from BQ
+2. Map real values to fake values
+3. Switch Real values with fake values
+4. insert back into bq
 
 
 
---
+++ fakerexample.py
@@ -0,0 +1,11 @@
+from faker import Faker
+from faker.providers import company
+from faker.providers import internet
+
+#Create faker factory
+fake = Faker()
+
+## Want to make a service that allows people to
+1. 
+
+
